5f84a16a7fcd79904e9639aef48c173cfd3444b1
GrzegorzB <g.bogusz93@gmail.com>
2015-12-06 22:55:24 +0100

    Spring security with database.

--- src/main/java/pl/boguszg/impulse/HomeController.java
+++ src/main/java/pl/boguszg/impulse/HomeController.java
@@ -2,6 +2,10 @@ package pl.boguszg.impulse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -68,6 +72,25 @@ public class HomeController {
 		model.setViewName("login");
 
 		return model;
+
+	}
+	
+	
+	//for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
+
+	  ModelAndView model = new ModelAndView();
+		
+	  //check if user is login
+	  Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	  if (!(auth instanceof AnonymousAuthenticationToken)) {
+		UserDetails userDetail = (UserDetails) auth.getPrincipal();	
+		model.addObject("username", userDetail.getUsername());
+	  }
+		
+	  model.setViewName("403");
+	  return model;
 
 	}
 
